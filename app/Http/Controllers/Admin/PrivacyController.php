8ca85408322a93f1883cf051fd2a1a6256e4789f
Mohamedkhaled46 <mookhaled46@gmail.com>
2023-07-06 03:15:16 +0300

    go to eid

---
+++ app/Http/Controllers/Admin/PrivacyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PrivacyController extends Controller
+{
+    public function addprivacy()
+    {
+        return view('privacy.add');
+    }
+}
