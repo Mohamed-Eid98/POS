8596fc2444e5fec3c5b25cff0bee61c01260b520
Mohamed-Eid98 <m.eid.saad403@gmail.com>
2023-07-16 18:59:31 +0300

    add color form

--- resources/views/product/addcolorandsize.blade.php
+++ resources/views/product/addcolorandsize.blade.php
@@ -8,24 +8,13 @@
     <!-- Plugins css -->
     <link href="{{ URL::asset('assets/plugins/owl-carousel/owl.carousel.css') }}" rel="stylesheet" />
     <!-- Maps css -->
-    <link href="{{ URL::asset('assets/plugins/jqvmap/jqvmap.min.css') }}" rel="stylesheet">
 
 
     <link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet"
         type="text/css">
-    <link href="{{ asset('assets/libs/spectrum-colorpicker2/spectrum.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/libs/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet"
-        type="text/css">
-    <link href="{{ asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/libs/@chenfengyuan/datepicker/datepicker.min.css') }}">
 
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('build/libs/dropzone/min/dropzone.min.css') }}" rel="stylesheet" type="text/css" />
+
 @endsection
 
 
@@ -58,21 +47,20 @@
 
                     <div>
 
-                        <form action="{{ route('product.addcolorandsize.store') }}" class="dropzone" method="POST"
-                            enctype="multipart/form-data">
+                        <form action="{{ route('product.addcolorandsize.store') }}" method="POST" class="dropzone" >
                             @csrf
 
 
                             <div class="col-md-12">
 
                                 <div class="form-group">
-                                    <h4>ادخل اللون<span class="text-danger">*</span></h4>
+                                    <h4>ادخل لون جديد<span class="text-danger">*</span></h4>
                                     <div class="controls">
 
                                         <div class="controls">
-                                            <input type="colour" name="price" class="form-control" />
-                                            @error('colour')
-                                                <span class="text-danger">{{ $message }}</span>
+                                            <input type="text" name="color" class="form-control my-3" />
+                                            @error('color')
+                                                <span class="text-danger"><strong>{{ $message }}</strong></span>
                                             @enderror
                                         </div>
 
@@ -81,15 +69,14 @@
                                 </div>
 
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
 
                                 <div class="mb-3">
-                                    <h4 class="form-label">المقاس </h4>
-
+                                    <h4>ادخل مقاس جديد<span class="text-danger">*</span></h4>
                                     <div class="controls">
-                                        <input type="size" name="price" class="form-control" />
+                                        <input type="text" name="size" class="form-control my-2" />
                                         @error('size')
-                                            <span class="text-danger">{{ $message }}</span>
+                                            <span class="text-danger"><strong>{{ $message }}</strong></span>
                                         @enderror
                                     </div>
 
@@ -118,72 +105,150 @@
             </div>
         </div> <!-- end col -->
     </div> <!-- end row -->
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-
-    <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/spectrum-colorpicker2/spectrum.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap-timepicker/js/bootstrap-timepicker.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/@chenfengyuan/datepicker/datepicker.min.js') }}"></script>
-
-    <!-- form advanced init -->
-    <script src="{{ asset('assets/js/pages/form-advanced.init.js') }}"></script>
-
-    <script src="{{ asset('assets/js/app.js') }}"></script>
-
-    {{-- <script type="text/javascript">
-        function mainThamUrl(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    $('#mainThmb').attr('src', e.target.result).width(130).height(150);
-                };
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-    </script> --}}
 
 
-    <script>
-        $(document).ready(function() {
-            $('#multiImg').on('change', function() { //on file input change
-                if (window.File && window.FileReader && window.FileList && window
-                    .Blob) //check File API supported browser
-                {
-                    var data = $(this)[0].files; //this file data
 
-                    $.each(data, function(index, file) { //loop though each file
-                        if (/(\.|\/)(gif|jpe?g|png)$/i.test(file
-                                .type)) { //check supported file type
-                            var fRead = new FileReader(); //new filereader
-                            fRead.onload = (function(file) { //trigger function on successful read
-                                return function(e) {
-                                    var img = $('<img/>').addClass('thumb').attr('src',
-                                            e.target.result).width(80)
-                                        .height(80); //create image element
-                                    $('#preview_image').append(
-                                        img); //append image to output element
-                                };
-                            })(file);
-                            fRead.readAsDataURL(file); //URL representing the file's data.
-                        }
-                    });
 
-                } else {
-                    alert("Your browser doesn't support File API!"); //if File API is absent
-                }
-            });
-        });
-    </script>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
 
-    {{-- <script src="{{ asset('build/libs/dropzone/min/dropzone.min.js') }}"></script> --}}
+                    <h4 class="card-title">عرض الالوان</h4>
+                    <p class="card-title-desc">
 
-    {{-- <script src="{{ asset('build/js/app.js') }}"></script> --}}
-@endsection
+                    </p>
+
+                    <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                        <div class="row">
+
+
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table id="example" class="table table-striped my-3 w-100" role="grid"
+                                    aria-describedby="datatable_info">
+                                    <thead>
+                                        <tr role="row">
+                                            <th>#</th>
+                                            {{-- <th>المحافظات</th> --}}
+                                            <th>الاسم</th>
+
+                                            <th>التعديلات</th>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody>
+                                        <?php $i = 0; ?>
+
+                                        <?php $i++; ?>
+@foreach ($colors as $color)
+
+                                        <tr>
+                                            <td>{{ $i }}</td>
+                                            <td> {{ $color->name }} </td>
+
+
+                                            <td>
+                                                <ul class="list-unstyled hstack gap-1 mb-0">
+                                                    {{-- <li data-bs-toggle="tooltip" data-bs-placement="top" title="تعديل ">
+                                                        <a href="" class="btn btn-sm btn-soft-primary"><i
+                                                                class="mdi mdi-pencil-outline"></i></a>
+                                                    </li> --}}
+
+                                                    <li data-bs-toggle="tooltip" data-bs-placement="top" title="حذف">
+                                                        <a href="" title="حذف"
+                                                            class="btn btn-sm btn-soft-danger"><i
+                                                                class="mdi mdi-delete-outline"></i></a>
+                                                    </li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+@endforeach
+
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+
+                    <h4 class="card-title">عرض الاحجام</h4>
+                    <p class="card-title-desc">
+
+                    </p>
+
+                    <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                        <div class="row">
+
+
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table id="example" class="table table-striped my-3 w-100" role="grid"
+                                    aria-describedby="datatable_info">
+                                    <thead>
+                                        <tr role="row">
+                                            <th>#</th>
+                                            {{-- <th>المحافظات</th> --}}
+                                            <th>الحجم</th>
+
+                                            <th>التعديلات</th>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody>
+                                        <?php $i = 0; ?>
+
+                                        <?php $i++; ?>
+@foreach ($sizes as $size)
+
+                                        <tr>
+                                            <td>{{ $i }}</td>
+                                            <td> {{ $size->name }} </td>
+
+
+                                            <td>
+                                                <ul class="list-unstyled hstack gap-1 mb-0">
+                                                    {{-- <li data-bs-toggle="tooltip" data-bs-placement="top" title="تعديل ">
+                                                        <a href="" class="btn btn-sm btn-soft-primary"><i
+                                                                class="mdi mdi-pencil-outline"></i></a>
+                                                    </li> --}}
+
+                                                    <li data-bs-toggle="tooltip" data-bs-placement="top" title="حذف">
+                                                        <a href="" title="حذف"
+                                                            class="btn btn-sm btn-soft-danger"><i
+                                                                class="mdi mdi-delete-outline"></i></a>
+                                                    </li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+@endforeach
+
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+    @endsection
+
+
+
+
+
+
